edec2fa12a32a52d2ec3f6337a36165c5dc0db77
Vinícius coêlho <coelho.provnc@gmail.com>
2021-11-25 13:25:38 -0300

    Fim da seção Estrutura de Controle

---
+++ controle/while.js
@@ -0,0 +1,13 @@
+function getInteiroALeatorioEntre(min, max) {
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
+}
+
+let opcao = 0;
+
+while (opcao != -1) {
+  opcao = getInteiroALeatorioEntre(-1, 10);
+  console.log(`A opção escolhida foi ${opcao}`);
+}
+
+console.log("Até a próxima!");
